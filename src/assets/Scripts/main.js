f9f04a86641b52deaf9b1835047235dca9ad1e52
Jean Grijp <grijpjean@gmail.com>
2021-01-19 23:18:06 -0300

    .

--- src/assets/Scripts/main.js
+++ src/assets/Scripts/main.js
@@ -9,7 +9,10 @@ mongoose.connect(process.env.CHAVE1, {useNewUrlParser: true, useUnifiedTopology:
     app.emit('DBok')
 }).catch(e => console.log(e))
 
+
 app.use(express.urlencoded({extended: true}))
+console.log(path.resolve(__dirname, '..', '..', '..', 'public'))
+app.use(express.static(path.resolve(__dirname, '..', '..', '..', 'public')))
 app.use(express.json())
 app.use(routes)
 // app.set('views', path.resolve(__dirname, 'src', 'views', 'HTML'))

044312cc5d63286d5c9cea7fa76cafa812c6092b
Jean Grijp <grijpjean@gmail.com>
2021-01-21 11:18:59 -0300

    .

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+
 const route = express.Router()
 const loginController = require('./src/controllers/loginController')
 const cadastroController = require('./src/controllers/cadastroController')
--- src/controllers/cadastroController.js
+++ src/controllers/cadastroController.js
@@ -4,18 +4,17 @@ const CadastroModel = require('../models/CadastroModel');
 
 exports.inicial = (req, res) => {
     res.sendFile(path.join(__dirname, '../views/HTML/cadastro.html'))
+    console.log(req.body)
 }
 
 
 exports.criarCadastro = (req, res) => {
-    console.log(req.json(req.body))
-    // res.send(req.body)
-    // CadastroModel.create({
-    //     nome: req.body.nome,
-    //     telefone: req.body.telefone,
-    //     email: req.body.email,
-    //     contatos: req.body.contatos,
-    //     usuario: req.body.usuario,
-    //     senha: req.body.senha
-    // }).then(dados => console.log(dados).catch(e => console.log(e)))
+    CadastroModel.create({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        email: req.body.email,
+        contatos: req.body.contatos,
+        usuario: req.body.usuario,
+        senha: req.body.senha
+    }).then(dados => console.log(dados).catch(e => console.log(e)))
 }

7e0d911d4ef892738ff60e8000d5107a8ae9511c
Jean Grijp <grijpjean@gmail.com>
2021-01-22 22:12:41 -0300

    .

--- src/controllers/cadastroController.js
+++ src/controllers/cadastroController.js
@@ -5,29 +5,14 @@ const CadastroModel = require('../models/CadastroModel');
 
 exports.inicial = (req, res) => {
     res.sendFile(path.join(__dirname, '../views/HTML/cadastro.html'))
-    console.log(req.body)
 }
-
 
 exports.register = async function(req, res) {
   try {
     const login = new Login(req.body);
     await login.register();
-
-    if(login.errors.length > 0) {
-      req.flash('errors', login.errors);
-      req.session.save(function() {
-        return res.redirect('back');
-      });
-      return;
-    }
-
-    req.flash('success', 'Seu usuário foi criado com sucesso.');
-    req.session.save(function() {
-      return res.redirect('back');
-    });
   } catch(e) {
     console.log(e);
     return res.sendFile(path.join(__dirname, '../views/HTML/cadastro.html'));
   }
-  };
+};

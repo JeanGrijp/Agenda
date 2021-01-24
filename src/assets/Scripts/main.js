4ca6ec8fccd0cada004bdfec0e12883e6f1b8435
Jean Grijp <grijpjean@gmail.com>
2021-01-24 14:23:17 -0300

    .

--- src/assets/Scripts/main.js
+++ src/assets/Scripts/main.js
@@ -4,6 +4,11 @@ const app = express()
 const path = require('path')
 const routes = require('../../../routes')
 const mongoose = require('mongoose')
+const session = require("express-session")
+const flash = require("connect-flash")
+const MongoStore = require('connect-mongo')(session)
+
+
 mongoose.connect(process.env.CHAVE1, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     console.log('Database conectada')
     app.emit('DBok')
@@ -15,6 +20,19 @@ console.log(path.resolve(__dirname, '..', '..', '..', 'public'))
 app.use(express.static(path.resolve(__dirname, '..', '..', '..', 'public')))
 app.use(express.json())
 app.use(routes)
+const sessionOptions = session({
+    secret: "shausghu sahughas 1454 4s54 @@@@@ #",
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 2,
+        httpOnly: true
+    }
+});
+app.use(sessionOptions);
+app.use(flash());
+
 // app.set('views', path.resolve(__dirname, 'src', 'views', 'HTML'))
 // app.set('view engine', 'ejs')
 // app.engine('html', require('html'))

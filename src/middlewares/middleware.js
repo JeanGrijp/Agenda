236bdf9da531b676f0a3ff9c1397077f42a2d908
Jean Grijp <grijpjean@gmail.com>
2021-01-26 11:54:27 -0300

    .

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -7,24 +7,24 @@ exports.middlewareGlobal = (req, res, next) => {
     next();
 };
 
-exports.checkCsrfError = (err, req, res, next) => {
-if(err) {
-    return res.sendFile(path.join(__dirname, '../views/HTML/404.html'));
-    }
-    next();
-};
+// exports.checkCsrfError = (err, req, res, next) => {
+// if(err) {
+//     return res.sendFile(path.join(__dirname, '../views/HTML/404.html'));
+//     }
+//     next();
+// };
 
-exports.csrfMiddleware = (req, res, next) => {
-    res.locals.csrfToken = req.csrfToken();
-    next();
-};
+// exports.csrfMiddleware = (req, res, next) => {
+//     res.locals.csrfToken = req.csrfToken();
+//     next();
+// };
 
-exports.loginRequired = (req, res, next) => {
-if(!req.session.user) {
-    req.flash('errors', 'Você precisa fazer login.');
-    req.session.save(() => res.redirect('/'));
-    return;
-    }
-    next();
-};
+// exports.loginRequired = (req, res, next) => {
+// if(!req.session.user) {
+//     req.flash('errors', 'Você precisa fazer login.');
+//     req.session.save(() => res.redirect('/'));
+//     return;
+//     }
+//     next();
+// };
 

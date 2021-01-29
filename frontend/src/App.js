a88bd787d37eee12697fc2e23ef966a6945f4a1d
Jean Grijp <grijpjean@gmail.com>
2021-01-29 18:57:44 -0300

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      hello world!!!!
+      <form action="" method="post">
+        <h1>Login</h1>
+        <input type="email" name="" placeholder="Login" id=""/>
+        <input type="password" name="" placeholder="Senha" id=""/>
+        <input type="submit" value="Login"/>
+      </form>
     </div>
   );
 }

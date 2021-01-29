bb75175311c18dd6cfc03bcdf547b27c9b5742e2
Jean Grijp <grijpjean@gmail.com>
2021-01-29 20:31:50 -0300

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import api from './services/api'
 import './App.css';
 
+import google from "./imagens/google.svg"
+
 function App() {
   return (
     <div className="container">
       <form action="" method="post">
-        <h1>Login</h1>
+        <h1>Sign In</h1>
         <input type="email" name="" placeholder="Login" id=""/>
         <input type="password" name="" placeholder="Senha" id=""/>
+        <a className="forgetPassword" href="">Forget passord?</a>
         <input type="submit" value="Login"/>
+        <p>Don't have a account? <a href="">Sign Up</a> </p>
+        <div className="google">
+          <img src={google} alt="" srcset=""/>
+          <input type="button" value="Or Login with Google"/>
+
+        </div>
       </form>
     </div>
   );

dcd78c4c11cc7e082cb80a04a5ced3eea4eba527
Jean Grijp <grijpjean@gmail.com>
2021-01-29 01:55:56 -0300

    .

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import api from './services/api'
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      hello world!!!!
+    </div>
+  );
+}
+
+export default App;

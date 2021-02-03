9697c62019a8660a2bc5bf11c4938d950a86ea52
Jean Grijp <grijpjean@gmail.com>
2021-02-03 00:54:04 -0300

    .

--- src/controllers/tarefaController.js
+++ src/controllers/tarefaController.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const User = require('../models/UserModel');
 const Tarefa = require('../models/TarefasModel');
 
@@ -5,14 +6,18 @@ module.exports = {
 
   async update(req, res) {
     // console.log(req.headers);
-    const { id } = req.headers;
-    const { description } = req.headers;
-    console.log();
-    // console.log(req.headers);
-    const tarefa = await Tarefa.findOneAndUpdate(id, description, {
+    const { _id } = req.headers;
+    // console.log(_id);
+    const before = await Tarefa.findOne({ _id });
+    console.log(before);
+    mongoose.set('useFindAndModify', false);
+    const desc = req.query.description;
+    console.log(desc);
+    const tarefa = await Tarefa.findOneAndUpdate({ _id }, { description: desc }, {
       returnOriginal: false,
       new: true,
     });
+    console.log();
     console.log(tarefa);
     return res.json(tarefa);
   },
